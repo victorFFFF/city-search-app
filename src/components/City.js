1bfee73e92074da471c1289ab469d77b6565218e
ryan4summer <ryan4summer0527@gmail.com>
2020-06-04 14:55:00 -0400

    add input city and to uppercase

--- src/components/City.js
+++ src/components/City.js
@@ -4,44 +4,59 @@ import axios from "axios";
 class City extends Component {
   constructor(props) {
     super(props);
-    this.state = { cityName: null };
+    this.state = { cityName: "" };
   }
 
-  componentDidMount() {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/" + this.props.name)
-      .then((response) => {
-        const data = response.data;
+  handleSubmit(){
 
-        const newCityName = {
-          name: data.name,
-          imageUrl: data.sprites.front_default,
-        };
-
-        this.setState({ cityName: newCityName });
-      })
-      .catch((err) => console.log(err));
   }
+
+  handleCityName = (evt)=>{
+    let input = evt.target.value;
+    let output = input.toUpperCase();
+    this.setState({cityName: output});
+    console.log(this.state.cityName);
+  }
+
+  // componentDidMount() {
+  //   axios
+  //     .get("https://pokeapi.co/api/v2/pokemon/" + this.props.name)
+  //     .then((response) => {
+  //       const data = response.data;
+
+  //       const newCityName = {
+  //         name: data.name,
+  //         imageUrl: data.sprites.front_default,
+  //       };
+
+  //       this.setState({ cityName: newCityName });
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
 
   render() {
     let display;
-    if (!this.state.pokemon) {
-      display = <p>Loading...</p>;
-    } else {
+    
       display = (
         <>
-          <img
-            src={this.state.pokemon.imageUrl}
-            alt={this.state.pokemon.name}
-          />
           <ul>
-            <li>{this.state.pokemon.name} </li>
+            <li>{this.state.cityName} </li>
           </ul>
         </>
       );
-    }
+    
 
-    return <div className="CityName">{display}</div>;
+    return (
+      <div>
+        <div> 
+        <label>Input city name:</label>
+          <input type="text" name="city" placeholder={this.state.cityName} onChange = {this.handleCityName} />
+          <button onClick={() => this.handleSubmit(this.state)}>submit</button>
+        </div>
+        <br></br>
+        <div className="CityName">Output the zip for city:{display}</div>
+      </div>
+    );
   }
 }
 
